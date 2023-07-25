2be9c65f5c0a2fbd5b86fa60bd974f24c8963162
Jacob Tang <jtang320@student.fuhsd.org>
2023-07-24 19:15:48 -0700

    added ingredient and instruction layouts

---
+++ app/src/main/java/com/example/randommealplanner/InstructionScreen.kt
@@ -0,0 +1,18 @@
+package com.example.randommealplanner
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class InstructionScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.instruction_screen)
+
+        //val button: Button = findViewById(R.id.buttonTitle)
+
+        //button.setOnClickListener { yellowButtonClicked() }
+
+
+    }
+}
