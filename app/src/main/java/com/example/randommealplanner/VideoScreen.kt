1557b3af7228d1d2a55a16c7c95c664a241e4c39
Jacob Tang <jtang320@student.fuhsd.org>
2023-07-31 18:25:19 -0700

    youtube link embedded

--- app/src/main/java/com/example/randommealplanner/VideoScreen.kt
+++ app/src/main/java/com/example/randommealplanner/VideoScreen.kt
@@ -1,6 +1,8 @@
 package com.example.randommealplanner
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class VideoScreen:AppCompatActivity() {
@@ -9,8 +11,11 @@ class VideoScreen:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.video_screen)
 
+        val youtubeTextView = findViewById<TextView>(R.id.youtubeLink)
+
         val intent = intent
         var videoURL = intent.getStringExtra("video")
 
+        youtubeTextView.text = videoURL
     }
 }
