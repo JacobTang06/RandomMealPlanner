e410ae90f34fb7a7eb63ad354695c256a1a2dbf8
Jacob Tang <jtang320@student.fuhsd.org>
2023-08-01 03:49:40 -0700

    youtube video shown

--- app/src/main/java/com/example/randommealplanner/VideoScreen.kt
+++ app/src/main/java/com/example/randommealplanner/VideoScreen.kt
@@ -1,21 +1,38 @@
 package com.example.randommealplanner
 
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class VideoScreen:AppCompatActivity() {
 
+    lateinit var youtubePlayerView: YouTubePlayerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.video_screen)
 
-        val youtubeTextView = findViewById<TextView>(R.id.youtubeLink)
+        youtubePlayerView = findViewById(R.id.youtube_player)
 
         val intent = intent
         var videoURL = intent.getStringExtra("video")
 
-        youtubeTextView.text = videoURL
+        val uri: Uri =
+            Uri.parse(videoURL)
+        val id = uri.getQueryParameter("id")
+
+        lifecycle.addObserver(youtubePlayerView)
+
+        youtubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                youTubePlayer.loadVideo("" + id, 0f)
+            }
+        })
+
     }
 }
